12d74a16116ff01f493371d4343aa2a15e637015
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2018-10-08 17:13:24 -0500

    f

--- ServerStuff/serverTest.py
+++ ServerStuff/serverTest.py
@@ -75,7 +75,7 @@ def runServer(sub, stream):
     @app.route('/update/<id>/<action>/action')
     def unsubscribe(id, action):
         if action == "unsubscribe":
-            sub.unsubscribe(stream)
+            sub.unsubscribe(stream, 'weird')
         elif action == "resetall":
             sub.reset_all(stream)
         elif action == "mute":
