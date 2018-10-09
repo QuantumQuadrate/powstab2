70bd80dbcf8ef7aa0697885c4e663e3832315efe
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2018-10-09 10:20:28 -0500

    f

--- actionHandler.py
+++ actionHandler.py
@@ -134,12 +134,17 @@ def PID_Handler(sub_sock, global_err_state, last_msg, log, pids, subscriptions, 
 
     try:
         [streamID, content] = sub_sock.recv_multipart()
+        print "\n \n \n"
+        print "DATA ACQUIRED"
+        print streamID
+        print content
+        print "\n \n \n"
+
         last_msg = time.time()
         try:
             log.debug("new data")
             for cb in subscriptions[streamID]:
                 result = cb['callback'](streamID, json.loads(content), log, **cb['kwargs'])
-                print result
                 pid_ctrl_name = result['name']
                 # check if pid controller exists
                 if pid_ctrl_name not in pids:
