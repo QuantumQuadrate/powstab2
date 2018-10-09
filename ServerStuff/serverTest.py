ef5ecd82cf0788241fadd974f13c67df84cdac31
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2018-10-09 13:54:14 -0500

    f

--- ServerStuff/serverTest.py
+++ ServerStuff/serverTest.py
@@ -70,9 +70,6 @@ def runServer(sub, stream, conMan):
             pause = 'Started'
 
         configStuff = conMan.config.items('CHANNEL'+str(id))
-        print configStuff
-        print configStuff[1][1]
-
         return render_template('keywords.html', id=id, kw_dict=kwargs, alert=alert, pause=pause, config_dict=configStuff)
 
     #subscribe
