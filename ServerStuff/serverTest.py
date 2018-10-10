c9ce5cfc45bbefd908675c583095a850948ec4a2
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2018-10-10 11:18:21 -0500

    f

--- ServerStuff/serverTest.py
+++ ServerStuff/serverTest.py
@@ -4,6 +4,7 @@ from flask import render_template
 from flask import request
 import json
 
+
 def runServer(sub, stream, conMan):
     # web server
     app = Flask(__name__)
@@ -16,7 +17,7 @@ def runServer(sub, stream, conMan):
         f.close()
     # commands & webpage
     # home page "monitor"
-    @app.route('/monitor', methods=['GET','PUT'])
+    @app.route('/monitor', methods=['GET', 'PUT'])
     def monitor():
         # GET request string(json) needs to be save as file, to be read by flask template
         if request.method == "PUT":
@@ -31,13 +32,13 @@ def runServer(sub, stream, conMan):
         # except Exception:
         #     return 'Unable to load page'
 
-    #subscribe
+    # subscribe
     @app.route('/update/<id>', methods=['GET', 'POST'])
     def update(id):
-        id=id.encode('ascii','ignore')
+        id = id.encode('ascii', 'ignore')
         with open(sub_file, 'r') as f:
             sub_list = json.load(f)
-            #sub_list = {1:{'kwargs':{kwargs}, 'control':{control}}
+            # sub_list = {1:{'kwargs':{kwargs}, 'control':{control}}
 
         if request.method == 'POST':
             sub_list[id]['kwargs'] = request.form.to_dict()
@@ -47,12 +48,11 @@ def runServer(sub, stream, conMan):
         control = id_dict['control']
         kwargs = id_dict['kwargs']
 
-        if control['alert'] == True:
+        if control['alert']:
             alert = 'On'
         else:
             alert = 'Off'
-
-        if control['pause'] == True:
+        if control['pause']:
             pause = 'Paused'
         else:
             pause = 'Started'
@@ -61,7 +61,7 @@ def runServer(sub, stream, conMan):
 
         return render_template('keywords.html', id=id, kw_dict=kwargs, alert=alert, pause=pause, config_dict=configStuff)
 
-    #subscribe
+    # subscribe
     @app.route('/update/<id>/config', methods=['POST'])
     def updateConfig(id):
         configDict = request.form.to_dict()
@@ -69,7 +69,6 @@ def runServer(sub, stream, conMan):
             conMan.config.set('CHANNEL'+str(id), key, configDict[key])
         conMan.updateConfig()
         return ''
-
 
     @app.route('/update/<id>/<action>/action', methods=['POST'])
     def unsubscribe(id, action):
@@ -100,4 +99,3 @@ def runServer(sub, stream, conMan):
         return ''
 
     app.run(host='0.0.0.0', debug=True)
-    logger.info('closing')
