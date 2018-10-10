88c741867646e96cd4368151a51f57d357ec0436
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2018-10-10 16:29:23 -0500

    t

--- actionHandler.py
+++ actionHandler.py
@@ -184,14 +184,14 @@ class PID_Handler(object):
                         self.log.debug('recieved first instance from channel: {} type: {}'.format(pid_ctrl_name, fb_type))
                         if fb_type == WK10CR1.type:
                             self.pids[pid_ctrl_name]['pid'] = WK10CR1(result['channel'], conMan.config, logger=self.log)
-                            # if self.flag == 0:
-                            #     print "\n\n\n\n\n"
-                            #     print "reached this point"
-                            #     print "\n\n\n\n\n"
-                            #     self.flag = 1
-                            #     testClient = testWorker([self.pids[pid_ctrl_name]['pid']])
-                            #     testClient.startServer()
-                            #     thread.start_new_thread(testClient.streamData(), ())
+                            if self.flag == 0:
+                                print "\n\n\n\n\n"
+                                print "reached this point"
+                                print "\n\n\n\n\n"
+                                self.flag = 1
+                                testClient = testWorker([self.pids[pid_ctrl_name]['pid']])
+                                testClient.startServer()
+                                thread.start_new_thread(testClient.streamData(), ())
                         if fb_type == WDAC8532.type:
                             self.pids[pid_ctrl_name]['pid'] = WDAC8532(result['channel'], conMan.config, logger=self.log)
                     # update with new info, save error state
