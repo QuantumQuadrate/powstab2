f2c83e66ed61e3a8a849bee300c874281939e034
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2018-10-11 11:42:27 -0500

    f

--- actionHandler.py
+++ actionHandler.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 from worker_K10CR1 import WK10CR1
 from worker_DAC8532 import WDAC8532
 import requests
-stream_filter = ''
+
 
 
 class generic_Handler(object):
@@ -21,15 +21,15 @@ class generic_Handler(object):
         self.sub_sock = sub_sock
         if cmd['action'] == 'SUBSCRIBE':
             msg = 'Subscribing with stream filter: [{}]'
-            stream_filter = cmd['stream_filter']
-            self.log.info(msg.format(stream_filter))
+            self.stream_filter = cmd['stream_filter']
+            self.log.info(msg.format(self.stream_filter))
 
             # add the callback to the list of things to do for the stream
-            if stream_filter not in self.subscriptions:
-                self.subscriptions[stream_filter] = []
+            if self.stream_filter  not in self.subscriptions:
+                self.subscriptions[self.stream_filter] = []
                 # stream_filter is assigned as a key with an empty list
-                self.sub_sock.setsockopt_string(zmq.SUBSCRIBE, stream_filter)
-            self.subscriptions[stream_filter].append({
+                self.sub_sock.setsockopt_string(zmq.SUBSCRIBE, self.stream_filter)
+            self.subscriptions[self.stream_filter].append({
                 'callback': cmd['callback'],
                 'kwargs': cmd['kwargs'],
                 'state': {},
@@ -42,12 +42,12 @@ class generic_Handler(object):
             self.sub_list[cmd['id']] = {
                 'kwargs': cmd['kwargs'],
                 'control': {'alert': True, 'pause': False}}
-            self.log.info("subscriptions: {}".format(self.subscriptions[stream_filter]))
+            self.log.info("subscriptions: {}".format(self.subscriptions[self.stream_filter]))
 
         if cmd['action'] == 'UPDATE_KW':
             msg = 'Updating channel...'
             self.log.info(msg.format(cmd['stream_filter']))
-            for cb in self.subscriptions[stream_filter]:
+            for cb in self.subscriptions[self.stream_filter]:
                 if cb['id'] == cmd['id']:
                     cb['kwargs'] = cmd['kwargs']
                     # self.sub_list = {1:{'kwargs':{kwargs}, 'control':{control}}
@@ -55,13 +55,13 @@ class generic_Handler(object):
                         'control': cb['control'],
                         'kwargs': cmd['kwargs']
                     }
-            self.log.info("subscriptions: {}".format(self.subscriptions[stream_filter]))
+            self.log.info("subscriptions: {}".format(self.subscriptions[self.stream_filter]))
 
         if (cmd['action'] == 'UNSUBSCRIBE' or
                 cmd['action'] == 'REMOVE_ALL_CBS'):
             msg = 'Unsubscribing to stream filter: [{}]'
             self.log.info(msg.format(cmd['stream_filter']))
-            self.sub_sock.setsockopt_string(zmq.UNSUBSCRIBE, stream_filter)
+            self.sub_sock.setsockopt_string(zmq.UNSUBSCRIBE, self.stream_filter)
 
         if cmd['action'] == 'REMOVE_ALL_CBS':
             msg = 'Removing all callbacks for stream filter: [{}]'
@@ -71,27 +71,27 @@ class generic_Handler(object):
         if cmd['action'] == 'RESET':
             msg = 'Resetting channel'
             self.log.info(msg.format(cmd['stream_filter']))
-            for cb in self.subscriptions[stream_filter]:
+            for cb in self.subscriptions[self.stream_filter]:
                 # cb is a dict with all the info of each channel subscribed
                 # stream_filter is a list of all the cb dict.
                 if cb['id'] == cmd['id']:
                     cb['state'] = {}
-            self.log.info("subscriptions: {}".format(self.subscriptions[stream_filter]))
+            self.log.info("subscriptions: {}".format(self.subscriptions[self.stream_filter]))
 
         if cmd['action'] == 'RESET_ALL':
             msg = 'Resetting all channels'
             self.log.info(msg.format(cmd['stream_filter']))
-            for cb in self.subscriptions[stream_filter]:
+            for cb in self.subscriptions[self.stream_filter]:
                 # cb is a dict with all the info of each channel subscribed
                 # stream_filter is a list of all the cb dict.
                 cb['state'] = {}
-            self.log.info("subscriptions: {}".format(self.subscriptions[stream_filter]))
+            self.log.info("subscriptions: {}".format(self.subscriptions[self.stream_filter]))
 
         if (cmd['action'] == 'MUTE' or
                 cmd['action'] == 'UNMUTE'):
             msg = 'Muted channel alert'
             self.log.info(msg.format(cmd['stream_filter']))
-            for cb in self.subscriptions[stream_filter]:
+            for cb in self.subscriptions[self.stream_filter]:
                 if cb['id'] == cmd['id']:
                     cb['control']['alert'] = (cmd['action'] == 'UNMUTE')
                     self.sub_list[cmd['id']] = {
@@ -103,7 +103,7 @@ class generic_Handler(object):
                 cmd['action'] == 'UNMUTE_ALL'):
             msg = 'Muted/Unmuted all channel alert'
             self.log.info(msg.format(cmd['stream_filter']))
-            for cb in self.subscriptions[stream_filter]:
+            for cb in self.subscriptions[self.stream_filter]:
                 cb['control']['alert'] = (cmd['action'] == 'UNMUTE_ALL')
                 self.sub_list[cb['id']] = {
                     'control': cb['control'],
@@ -114,7 +114,7 @@ class generic_Handler(object):
                 cmd['action'] == 'RESTART_ALL'):
             msg = 'Paused/ Restarted all channels'
             self.log.info(msg.format(cmd['stream_filter']))
-            for cb in self.subscriptions[stream_filter]:
+            for cb in self.subscriptions[self.stream_filter]:
                 cb['control']['pause'] = (cmd['action'] == 'PAUSE_ALL')
                 self.sub_list[cb['id']] = {
                     'control': cb['control'],
@@ -125,7 +125,7 @@ class generic_Handler(object):
                 cmd['action'] == 'RESTART'):
             msg = 'Paused/Restarted this channel'
             self.log.info(msg.format(cmd['stream_filter']))
-            for cb in self.subscriptions[stream_filter]:
+            for cb in self.subscriptions[self.stream_filter]:
                 if cb['id'] == cmd['id']:
                     cb['control']['pause'] = (cmd['action'] == 'PAUSE')
                     self.sub_list[cmd['id']] = {
@@ -134,7 +134,7 @@ class generic_Handler(object):
                     }
         sub_list_json = json.dumps(self.sub_list)
         requests.put('http://127.0.0.1:5000/monitor', json=sub_list_json)
-        return stream_filter
+        return self.stream_filter
 
 
 class PID_Handler(object):
