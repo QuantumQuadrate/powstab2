c649d5ca9a54fb862c65652fbf9001048db95e94
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2018-10-12 11:58:31 -0500

    fixed input writing

--- dataReader.py
+++ dataReader.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
+import urllib2
 
+data = urllib2.urlopen("http://192.168.137.2:5000/static/data.txt").read(20000) # read only 20 000 chars
+data = data.split("\n")
 
-dataTracker = open("http://192.168.137.2:5000/static/data.txt", "w+")
-data = dataTracker.read().split('\n')
-dataTracker.close()
 plt.plot(data)
 
 plt.show()
