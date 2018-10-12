96c4d622a2dd3d5bb11ea92b7b4e84c35d72e21c
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2018-10-12 12:28:58 -0500

    updating graph making

--- dataReader.py
+++ dataReader.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import urllib2
 import numpy as np
+import matplotlib.animation as animation
 import time
 flag = True
 fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
+ax1 = fig.add_subplot(1, 1, 1)
 while True:
     data = urllib2.urlopen("http://192.168.137.2:5000/static/data.txt").read(20000) # read only 20 000 chars
     data = data.split("\n")[:-1]
