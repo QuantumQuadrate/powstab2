ea0f342b894d13e6a2b70a5fd27b9df5068c2eb6
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2018-10-19 16:16:58 -0500

    fixes

--- testMatrix.py
+++ testMatrix.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 import ServerStuff.server as server
-import numpy as np
-import time
 
-a = [[0, 1],[1, 0]]
+a = [[0, 1], [1, 0]]
 dataStream = 'toy'
 outputStream = "BGTest"
 inputs = ['toy1', 'toy2']
@@ -12,6 +10,4 @@ outputs = ['output1', 'output2']
 
 myServer = server.MatrixTransformServer()
 myServer.setup(a, dataStream, inputs, outputs, outputStream)
-while True:
-    time.sleep(1)
-sub.close()
+myServer.runServer()
