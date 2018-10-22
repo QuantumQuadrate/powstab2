454ed8700b6a49761128b38530974ad15eae6345
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2018-10-22 13:40:36 -0500

    Fixing config file error

--- ServerStuff/server.py
+++ ServerStuff/server.py
@@ -103,4 +103,4 @@ def runServer(sub, stream, conMan):
             return 'error'
         return ''
 
-    app.run(host='0.0.0.0', debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', debug=True, use_reloader=False, port=80)
