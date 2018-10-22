a9f8b07c7cde826ef05dd57f897c214e6e9e0ff6
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2018-10-22 15:27:50 -0500

    testing different sizes

--- testMatrix.py
+++ testMatrix.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 import ServerStuff.server as server
 
-a = [[1, -1], [1, 1]]
+a = [[1, -1], [1, 1], [0, 0]]
 dataStream = 'toy'
 outputStream = "BGTest"
 inputs = ['toy1', 'toy2']
-outputs = ['diff', 'sum']
+outputs = ['diff', 'sum', 'zero']
 
 
 myServer = server.MatrixTransformServer()
